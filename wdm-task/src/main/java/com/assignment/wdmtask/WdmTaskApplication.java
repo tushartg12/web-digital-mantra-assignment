cf4605d7f809fd7cc0b2387eb3652b0a41d86111
Tushar <tushartg600@gmail.com>
2023-09-28 12:51:38 +0530

    initial commit

---
+++ wdm-task/src/main/java/com/assignment/wdmtask/WdmTaskApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.wdmtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdmTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdmTaskApplication.class, args);
+	}
+
+}
